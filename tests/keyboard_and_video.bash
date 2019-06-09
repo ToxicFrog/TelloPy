92feb526b97698a2e552155c01453ebdba36bd6c
B. Kelly <bk@ancilla.ca>
2019-06-09 13:25:53 -0400

    updated keyboard_and_video.bash

--- tests/keyboard_and_video.bash
+++ tests/keyboard_and_video.bash
@@ -3,9 +3,9 @@
 # ports 9000, 9617, 6038 -- 6038 is used for video stream, not sure about others
 
 PYTHON="${PYTHON:=python3}"
-#mkdir -p ~/Pictures/
+mkdir -p ~/Pictures/
 PYSITE="$("$PYTHON" -m site --user-site)"
 mkdir -p "$PYSITE"
-cd "$(dirname "$(readlink -f $0)")"
+cd "$(dirname "$(readlink -f $0)")/.."
 ln -sf "$(pwd)/tellopy" "$PYSITE/"
-python3 tellopy/examples/keyboard_and_video.py
+python3 -m tellopy.examples.keyboard_and_video
